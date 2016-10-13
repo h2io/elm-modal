727e420d20a76765e348d986321778771d120f31
Victor Ionescu <victor@dev.ro>
2016-10-13 15:13:35 +0300

    update example to latest API version & remove comments

--- example/App.elm
+++ example/App.elm
@@ -3,7 +3,7 @@ module App exposing (..)
 import Html exposing (div, button, text, h1, p, Html)
 import Html.Events exposing (onClick)
 import Html.App as App
-import H2ioModal exposing (modalModel)
+import H2ioModal
 
 
 main : Program Never
@@ -38,57 +38,28 @@ update msg model =
         NoOp ->
             model ! []
 
-        {--`Show` manually sends `H2ioModal.update` the `Show Msg` so you don't
-            handle more complex actions in your own app. Also, in this way you
-            may add your own `model` modifications here as well.
-            `H2ioModal.Close` is also exposed, in case you want to clean up your
-            own model after the `H2ioModal` closed
-        --}
         Show ->
-            let
-                modal' =
-                    H2ioModal.update H2ioModal.Show model.modal
-            in
-                { model | modal = modal' } ! []
+            { model | modal = H2ioModal.show model.modal } ! []
 
         H2ioModal msg' ->
-            let
-                modal' =
-                    H2ioModal.update msg' model.modal
-            in
-                { model | modal = modal' } ! []
+            { model | modal = H2ioModal.update msg' model.modal } ! []
 
 
-
-{--modalConfig takes the modal's model and just adds the custom values
-    necessary for your app. `content` should always be set by you.
-    `modalModel` is exposed in the import because the compiler has issues
-    destructuring capitalized records.
---}
-
-
-modalConfig : H2ioModal.Model
+modalConfig : H2ioModal.ViewModel Msg
 modalConfig =
-    { modalModel
-        | content = content
-        , width = "340px"
-        , height = "480px"
+    { content = content
+    , width = "340px"
+    , height = "480px"
     }
 
 
 model : Model
 model =
-    { modal = modalConfig
+    { modal = H2ioModal.init
     }
 
 
-
-{--Because the content will be passed down to the H2ioModal, it has to have a
-    `Html H2ioModal.Msg` type instead of an often used `Html Msg` type.
---}
-
-
-content : Html H2ioModal.Msg
+content : Html Msg
 content =
     div []
         [ h1 [] [ text "Example Modal" ]
@@ -100,5 +71,5 @@ view : Model -> Html Msg
 view model =
     div []
         [ button [ onClick Show ] [ text "Show Modal" ]
-        , H2ioModal.view model.modal |> App.map H2ioModal
+        , H2ioModal.view H2ioModal modalConfig model.modal
         ]
